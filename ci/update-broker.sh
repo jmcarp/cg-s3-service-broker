81e0c29e60a1d37718d963da6ab0ab1ffface606
Joshua Carp <jm.carp@gmail.com>
2016-06-01 01:12:36 -0400

    Deploy to production after staging acceptance tests.

--- ci/update-broker.sh
+++ ci/update-broker.sh
@@ -6,6 +6,7 @@ cf login -a $CF_API_URL -u $CF_DEPLOY_USERNAME -p $CF_DEPLOY_PASSWORD -o $CF_ORG
 
 BROKER_URL=https://`cf app $BROKER_NAME | grep urls: | sed 's/urls: //'`
 
+# Create or update service broker
 if ! cf create-service-broker $BROKER_NAME $AUTH_USER $AUTH_PASS $BROKER_URL ; then
   cf update-service-broker $BROKER_NAME $AUTH_USER $AUTH_PASS $BROKER_URL
 fi
